514bbf9f56920ec358eb584d8fc5f6741cee0176
imjaegyun <imij0522@gmail.com>
2025-08-12 16:52:39 +0900

    update

---
+++ pidnet-s_2xb6-120k_512x512-satellite_[0~10].py
@@ -0,0 +1,129 @@
+_base_ = [
+    '../_base_/datasets/satellite_512x512_[0~10].py',
+    '../_base_/default_runtime.py'
+]
+
+checkpoint_file = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/pidnet/pidnet-s_imagenet1k_20230306-715e6273.pth'
+crop_size = (1024, 1024)
+data_preprocessor = dict(
+    type='SegDataPreProcessor',
+    mean=[82.6953063, 87.2977321, 78.54846484],
+    std=[39.50921794, 34.6564753, 31.20142518],
+    bgr_to_rgb=True,
+    pad_val=0,
+    seg_pad_val=255,
+    size=crop_size
+)
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+model = dict(
+    type='EncoderDecoder',
+    data_preprocessor=data_preprocessor,
+    backbone=dict(
+        type='PIDNet',
+        in_channels=3,
+        channels=32,
+        ppm_channels=96,
+        num_stem_blocks=2,
+        num_branch_blocks=3,
+        align_corners=False,
+        norm_cfg=norm_cfg,
+        act_cfg=dict(type='ReLU', inplace=True),
+        init_cfg=dict(type='Pretrained', checkpoint=checkpoint_file)),
+    decode_head=dict(
+        type='PIDHead',
+        in_channels=128,
+        channels=128,
+        num_classes=2,
+        norm_cfg=norm_cfg,
+        act_cfg=dict(type='ReLU', inplace=True),
+        align_corners=True,
+        loss_decode=[
+            dict(
+                type='CrossEntropyLoss',
+                use_sigmoid=False,
+                loss_weight=0.4
+            ),
+            dict(
+                type='OhemCrossEntropy',
+                thres=0.9,
+                min_kept=131072,
+                loss_weight=1.0
+            ),
+            dict(type='DiceLoss', loss_weight=20.0),
+            dict(
+                type='OhemCrossEntropy',
+                thres=0.9,
+                min_kept=131072,
+                loss_weight=1.0
+            )
+        ]),
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole')
+)
+
+# optimizer
+iters = 30000
+optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0005)
+optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
+
+# learning policy
+param_scheduler = [
+    dict(
+        type='PolyLR',
+        eta_min=0,
+        power=0.9,
+        begin=0,
+        end=iters,
+        by_epoch=False)
+]
+
+# setting visualizer
+visualizer = dict(
+    type="Visualizer", 
+    vis_backends=[dict(type='LocalVisBackend'),
+                      dict(type="WandbVisBackend")]
+)
+
+# settiing logger
+log_config = dict(
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(
+            type='WandbLoggerHook',
+            init_kwargs={'project': 'mmsegmentation'},
+            interval=10,
+            log_dir='./work_dirs/PIDNet_log'
+        ),
+    ]
+)
+
+log_processor = dict(
+    type='LogProcessor',
+    window_size=50,
+    by_epoch=True
+)
+
+# setting trian test cfg
+train_cfg = dict(
+    type='IterBasedTrainLoop', 
+    max_iters=iters, 
+    val_interval=iters // 1
+)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+# setting hook
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(
+        type='CheckpointHook', 
+        interval=iters // 10, 
+        by_epoch=False
+    ),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    visualization=dict(type='SegVisualizationHook', draw=True, interval=1)
+)
+
+randomness = dict(seed=3500)
